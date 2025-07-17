2f5e7e5f3f9c06eb439deaf7cd51c44c1776f2c3
Donald Forbes <donald.forbes@temporal.io>
2025-07-17 12:24:53 +0100

    Adding spring boot base.

---
+++ src/main/java/com/demo/priority/service/PriorityServiceApplication.java
@@ -0,0 +1,13 @@
+package com.demo.priority.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PriorityServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PriorityServiceApplication.class, args);
+	}
+
+}
